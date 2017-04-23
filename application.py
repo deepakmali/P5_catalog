1015cf2ca295fd913d87e8847fae71e451e7cda6
Deepak <deepak3823@gmail.com>
2017-04-23 22:30:39 +0530

    Add userid association with created categories

--- application.py
+++ application.py
@@ -27,6 +27,25 @@ session = DBSession()
 app = Flask('__name__')
 
 
+# creating user and returning the id, if already exists, just return userid
+def create_user(login_session):
+    user = Users()
+    user.name = login_session['name']
+    user.email = login_session['email']
+    user.picture = login_session['picture']
+    session.add(user)
+    session.commit()
+    user = session.query(Users).filter_by(email=login_session['email']).one()
+    return user
+
+# get userid 
+def get_userid(login_session):
+    user = session.query(Users).filter_by(email=login_session['email']).first()
+    if not user:
+        user = create_user(login_session)
+    return user.id
+
+
 # Sign in functions start here
 @app.route('/login')
 def showLogin():
@@ -93,11 +112,13 @@ def gconnect():
     answer = requests.get(userinfo_url, params=params)
     data = json.loads(answer.text)
     # Getting users info in login session
-    login_session['username'] = data["name"]
+    login_session['provider'] = 'google'
+    login_session['name'] = data["name"]
     login_session['picture'] = data["picture"]
     login_session['email'] = data["email"]
-    print login_session['username']
-    return login_session['username']
+    # print login_session['name']
+    login_session['myapp_userid'] = get_userid(login_session)
+    return login_session['name']
 
 # Logging out users
 @app.route('/gdisconnect')
@@ -115,10 +136,11 @@ def gdisconnect():
     if result['status'] == '200':
         # Reset the user's session
         del login_session['credentials']
-        del login_session['username']
+        del login_session['name']
         del login_session['email']
         del login_session['picture']
         del login_session['gplus_id']
+        del login_session['myapp_userid']
 
         response = make_response(json.dumps('Successfully disconnected'), 200)
         response.headers['Content-Type'] = 'application/json'
@@ -128,6 +150,58 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
+
+# connecting with facebook. This will be called in the ajax call from login page
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = ('https://graph.facebook.com/oauth/access_token?'
+           'grant_type=fb_exchange_token&client_id=%s&client_secret=%s'
+           '&fb_exchange_token=%s') %(app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
+
+    data = json.loads(result)
+    token = 'access_token=' + data['access_token']
+
+    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # store the results
+    data = json.loads(result)
+    print data
+    login_session['provider'] = 'facebook'
+    # print data
+    login_session['email'] = data['email']
+    login_session['username'] = data['name']
+    login_session['facebook_id'] = data['id']
+    # The token must be stored in the login_session in order to properly logout, 
+    # let's strip out the information before the equals sign in our token
+    stored_token = token.split("=")[1]
+    login_session['access_token'] = stored_token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+    login_session['myapp_userid'] = get_userid(login_session)
+    return login_session['username']
+
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    pass
 
 
 # Application operation functions start here
@@ -149,7 +223,8 @@ def create_category():
         return render_template("new_category.html", category=None)
     else:
         category = request.form['new_category']
-        new_category = Categories(name=category)
+        new_category = Categories(name=category,
+                                  created_by=login_session['myapp_userid'])
         session.add(new_category)
         session.commit()
         return redirect(url_for('home'))
@@ -192,10 +267,10 @@ def create_item(category_name=None):
         return render_template('new_item.html',
                                category=category,
                                item=None,
-                               all_categories=all_categories
+                               all_categories=all_categories,
                                )
     else:
-
+        # created by is not required for items
         item_name = request.form['new_item_name']
         item_desc = request.form['new_item_desc']
         # print category.name
